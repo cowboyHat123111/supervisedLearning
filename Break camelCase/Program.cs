e0d3248e2af529c21e314335c3bd64c4b6ec32e2
cowboyHat123111 <cowboy.hat1@protonmail.com>
2024-08-22 17:25:52 +0300

    improve logging

---
+++ Break camelCase/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Break_camelCase
+{
+    internal class Program
+    {
+        public static string BreakCamelCase(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    str = str.Insert(i++, " ");
+                }
+            }
+            return str;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BreakCamelCase("helloWorld"));
+        }
+    }
+}
